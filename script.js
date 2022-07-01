51e8b21000be48caea5b5342717177dec0017d4b
RonnieMu34 <ronnie34mu@gmail.com>
2022-07-01 11:54:23 -0400

    Create function for one round of game play

--- script.js
+++ script.js
@@ -4,3 +4,41 @@ function computerPlay() {
     let choice = choices[randomIndex];
     return choice;
 }
+
+function playGame(computerChoice) {
+
+    playerChoice = prompt("Rock, Paper, or Scissors?");
+    playerChoice = playerChoice.toLowerCase();
+    
+    if (computerChoice == "rock") {
+        if (playerChoice == "paper") {
+            return "You win! Paper beats rock!";
+        } else if (playerChoice == "scissors") {
+            return "You lose! Scissors loses to rock!";
+        } else {
+            return "It's a tie! Both chose rock!";
+        }
+    }
+
+    if (computerChoice == "paper") {
+        if (playerChoice == "paper") {
+            return "It's a tie! Both chose paper!";
+        } else if (playerChoice == "scissors") {
+            return "You win! Scissors beats paper!";
+        } else {
+            return "You lose! Rock loses to paper!";
+        }
+    }
+
+    if (computerChoice == "scissors") {
+        if (playerChoice == "paper") {
+            return "You lose! Paper loses to scissors!";
+        } else if (playerChoice == "scissors") {
+            return "It's a tie! Both chose scissors!";
+        } else {
+            return "You win! Rock beats scissors!";
+        }
+    }
+}
+
+console.log(playGame(computerPlay()));
