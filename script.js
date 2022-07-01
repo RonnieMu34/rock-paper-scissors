537069726216694623c68702897a57a715d4468e
RonnieMu34 <ronnie34mu@gmail.com>
2022-07-01 10:42:37 -0400

    Create function for computer player
    This computer player function will randomly select either
    rock, paper, or scissors at random to play against the real
    player.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
 function computerPLay(choicesArray) {
-    
+    const randomIndex = Math.floor(Math.random() * choicesArray.length);
+    let choice = choicesArray[randomIndex];
+    return choice;
 }
