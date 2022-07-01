9a0372065c67b8f0c1ae88497949913848a2f612
RonnieMu34 <ronnie34mu@gmail.com>
2022-07-01 11:17:56 -0400

    Create function for computer to randomly select choice

---
+++ script.js
@@ -0,0 +1,7 @@
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random(), choices.length);
+    let choice = choices[randomIndex];
+    return choice;
+}
+
